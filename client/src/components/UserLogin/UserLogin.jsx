3bfda79f33466851aefd9f651dc5088c0be5d2c2
fasilkp <fasilkp314@gmail.com>
2023-03-29 10:39:23 +0530

    app initial setup completed

---
+++ client/src/components/UserLogin/UserLogin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserLogin() {
+  return (
+    <div className="login-main">
+        <nav className='login-nav'>
+            
+        </nav>
+    </div>
+  )
+}
+
+export default UserLogin
