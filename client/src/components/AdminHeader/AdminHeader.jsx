91ea813a320f6b40221365988d326539ced1732d
fasilkp <fasilkp314@gmail.com>
2023-03-31 12:20:19 +0530

    admin header ui created

--- client/src/components/AdminHeader/AdminHeader.jsx
+++ client/src/components/AdminHeader/AdminHeader.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
 import './AdminHeader.css'
 
@@ -5,10 +6,10 @@ function AdminHeader() {
   return (
     <div className="admin-header">
         <div className="admi-header-item">
-            <h3>DocOnline</h3>
+            <h5>DocOnline</h5>
         </div>
         <div className="admi-header-item">
-            
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 32, height: 32 }} />
         </div>
 
     </div>
