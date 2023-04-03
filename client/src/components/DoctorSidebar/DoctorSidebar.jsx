17c6858a1f3e43cd316adc000b59260a8df17fc4
fasilkp <fasilkp314@gmail.com>
2023-04-03 07:30:09 +0530

    doctor sidebar created

---
+++ client/src/components/DoctorSidebar/DoctorSidebar.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import '../AdminSidebar/Sidebar.css'
+import { RiArchiveDrawerLine, RiBuilding4Line, RiHome2Line, RiHospitalLine, RiUser2Line } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+
+function SidebarSidebar({ page }) {
+  return (
+    <div className="admin-sidebar">
+      <ul>
+        <Link to="/account/doctor/">
+          <li className={`admin-sideitems ${page == "dashboard" && 'active'}`}>
+            <div className='side'></div>
+            <div className="admin-sideItem">
+
+              <RiHome2Line className='icon' />
+              <span>Dashboard</span>
+            </div>
+          </li>
+        </Link>
+        <Link to="/account/doctor/schedule">
+
+          <li className={`admin-sideitems ${page == "schedule" && 'active'}`}>
+            <div className='side'></div>
+            <div className="admin-sideItem">
+
+              <RiArchiveDrawerLine className="icon" />
+              <span>Schedules</span>
+            </div>
+          </li>
+        </Link>
+        <Link to="/account/doctor/patient">
+
+          <li className={`admin-sideitems ${page == "patient" && 'active'}`}>
+            <div className='side'></div>
+            <div className="admin-sideItem">
+
+              <RiUser2Line className='icon' />
+              <span>Patients</span>
+            </div>
+          </li>
+        </Link>
+        {/* <Link to="/account/admin/hospitals">
+
+            <li className={`admin-sideitems ${page=="doctor request" && 'active'}`}>
+
+              <div className='side'></div>
+              <div className="admin-sideItem">
+
+                <RiBuilding4Line className='icon' />
+                <span>Doctor Requests</span>
+              </div>
+            </li>
+            </Link> */}
+        {/* <Link to="/account/admin/hospitals/requests">
+
+            <li className={`admin-sideitems ${page=="hospital request" && 'active'}`}>
+
+              <div className='side'></div>
+              <div className="admin-sideItem">
+
+                <RiHospitalLine className='icon' />
+                <span>Hospital Requests</span>
+              </div>
+            </li>
+            </Link> */}
+
+      </ul>
+
+    </div>
+  )
+}
+
+export default DoctorSideBar
---
+++ server/middlewares/verifyDoctor.js
@@ -0,0 +1,20 @@
+const verifyDoctor = async (req, res, next) => {
+    try {
+        const token = req.cookies.doctorToken;
+        if (!token)
+            return res.json({ loggedIn: false, error: true, message: "no token" });
+
+        const verifiedJWT = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const doctor = await DoctorModel.findOne({_id:verifiedJWT.id}, { password: 0 });
+        if (!doctor) {
+            return res.json({ loggedIn: false });
+        }
+        req.doctor=doctor;
+        next()
+    } catch (err) {
+        console.log(err)
+        res.json({ loggedIn: false, error: err });
+    }
+}
+
+export default verifyDoctor
