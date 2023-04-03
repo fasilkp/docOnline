f06fcbdc5bf376198519e4dc6544106f149b1c8e
fasilkp <fasilkp314@gmail.com>
2023-04-03 06:59:02 +0530

    doctor login and logout route completed

--- client/src/components/DoctorLogin/DoctorLogin.jsx
+++ client/src/components/DoctorLogin/DoctorLogin.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
 import { TextField } from '@mui/material';
 import loginImage from '../../assets/images/login.jpg'
 import "../UserLogin/userlogin.css"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { ClipLoader } from 'react-spinners';
+import axios from 'axios'
+import { useDispatch } from 'react-redux';
 
 function DoctorLogin() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errMessage, setErrMessage] = useState("")
+    const [loading, setLoading] = useState({
+        submit: false
+    })
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setLoading({ ...loading, submit: true })
+        if (validForm()) {
+            const { data } = await axios.post("/doctor/auth/login", {
+                email, password
+            })
+            if (data.err) {
+                setErrMessage(data.message)
+            } else {
+                dispatch({type:"refresh"})
+            }
+            setLoading({ ...loading, submit: false })
+
+        }
+
+    }
+    function validForm() {
+        if (email.trim() === "" || password.trim() === "") {
+            return false
+        }
+        return true
+    }
     return (
         <div className="login-main">
             <Row>
@@ -21,32 +55,41 @@ function DoctorLogin() {
                 <div className="login-container">
                     <Row>
 
-                    <Col md={6}>
-                        <div className="login-sec bg">
-                            <img src={loginImage} alt="" />
-                        </div>
-                    </Col>
-                    <Col md={6}>
-                        <div className="login-sec">
-                            <div className="login-box">
-                                <div className="login-row head">
-                                    <h3>Login</h3>
-                                </div>
-                                <div className="login-row w-100 mt-3">
-                                    <TextField id="outlined-basic" label="Email" type="email" variant="outlined" fullWidth className='input' />
-                                </div>
-                                <div className="login-row">
-                                    <TextField id="outlined-basic" label="Password" type="password" variant="outlined" className='input' fullWidth/>
-                                </div>
-                                <div className="login-row">
-                                    <button>login</button>
-                                </div>
-                                <div className="login-row mt-3">
-                                    <Link to="/signup">Don't Have an Account? Signin</Link>
-                                </div>
+                        <Col md={6}>
+                            <div className="login-sec bg">
+                                <img src={loginImage} alt="" />
                             </div>
-                        </div>
-                    </Col>
+                        </Col>
+                        <Col md={6}>
+                            <div className="login-sec">
+                                <form onSubmit={handleSubmit} className="login-box">
+                                    <div className="login-row head">
+                                        <h3>Login</h3>
+                                    </div>
+                                    <div className="login-row w-100">
+                                        <TextField id="outlined-basic" value={email} onChange={(e) => setEmail(e.target.value)} label="Email" type="email" variant="outlined" fullWidth className='input' />
+                                    </div>
+                                    <div className="login-row">
+                                        <TextField id="outlined-basic" value={password} onChange={(e) => setPassword(e.target.value)} label="Password" type="password" variant="outlined" className='input' fullWidth />
+                                    </div>
+                                    {
+                                        errMessage &&
+                                        <div className="login-row" style={{ justifyContent: "flex-start" }}>
+                                            <p className='text-danger'>{errMessage}</p>
+                                        </div>
+                                    }
+                                    <div className="login-row">
+                                        <button type='submit' disabled={!validForm() || loading.submit}>
+                                            Login
+                                            <ClipLoader size={20} color="white" loading={loading.submit} />
+                                        </button>
+                                    </div>
+                                    {/* <div className="login-row mt-3">
+                                        <Link to="/account/hospital/signup">Don't Have an Account? Signup</Link>
+                                    </div> */}
+                                </form>
+                            </div>
+                        </Col>
                     </Row>
                 </div>
             </Row>
---
+++ server/controllers/doctorAuthController.js
@@ -0,0 +1,64 @@
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcryptjs"
+import HospitalModel from '../models/HospitalModel.js';
+
+
+var salt = bcrypt.genSaltSync(10);
+
+
+export async function doctorLogin(req, res) {
+    try {
+        const { email, password } = req.body;
+        const doctor = await DoctorModel.findOne({ email})
+        if (!doctor){
+            return res.json({ err: true, message: "No Hospital Found" })
+        }
+    
+        const doctorValid = bcrypt.compareSync(password, doctor.password);
+        if (!doctorValid)
+            return res.json({ err: true, message: "wrong Password" })
+        const token = jwt.sign(
+            {
+                id: hospital._id
+            },
+            process.env.JWT_SECRET_KEY
+        )
+        return res.cookie("doctorToken", token, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            sameSite: "none",
+        }).json({ err: false })
+    }
+    catch (err) {
+        res.json({ message: "somrthing went wrong", error: err, err:true })
+    }
+}
+
+
+export const checkDoctorLoggedIn = async (req, res) => {
+    try {
+        const token = req.cookies.hospitalToken;
+        if (!token)
+            return res.json({ loggedIn: false, error: true, message: "no token" });
+
+        const verifiedJWT = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const doctor = await DoctorModel.findOne({_id:verifiedJWT.id}, { password: 0 });
+        if (!doctor) {
+            return res.json({ loggedIn: false });
+        }
+        return res.json({ hospital, loggedIn: true });
+    } catch (err) {
+        console.log(err)
+        res.json({ loggedIn: false, error: err });
+    }
+}
+
+export const doctorLogout = async (req, res) => {
+    res.cookie("hospitalToken", "", {
+        httpOnly: true,
+        expires: new Date(0),
+        secure: true,
+        sameSite: "none",
+    }).json({ message: "logged out", error: false });
+}
