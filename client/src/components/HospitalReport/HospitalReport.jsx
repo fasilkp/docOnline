a9992e8eef55f56add68a6d89c38e46c2fa11b38
fasilkp <fasilkp314@gmail.com>
2023-04-27 21:35:55 +0530

    hospital booking report ui completed

--- client/src/components/HospitalReport/HospitalReport.jsx
+++ client/src/components/HospitalReport/HospitalReport.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as React from 'react';
 import { useState } from 'react';
-import { Container, Dropdown, Table } from 'react-bootstrap';
+import { Col, Container, Dropdown, Row, Table } from 'react-bootstrap';
 import { RiMore2Fill, RiSearch2Line } from 'react-icons/ri';
 import Swal from 'sweetalert2'
 import { Backdrop, CircularProgress, setRef } from '@mui/material';
@@ -13,6 +13,11 @@ import { useEffect } from 'react';
 import EditDoctor from '../../Modal/editDoctor/EditDoctor';
 import { Link, useNavigate } from 'react-router-dom';
 import { getHospitalBookings } from '../../api/hospitalApi';
+import { DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import './HospitalReport.css'
 
 export default function HospitalReport() {
     const [refresh, setRefresh] = useState(false)
@@ -45,65 +50,184 @@ export default function HospitalReport() {
             <div className="admin-main">
                 <HospitalSidebar page={'reports'} clicked={clicked} />
                 <Container fluid>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
 
-                    <div className="admin-container">
-                        <div className="container-header">
-                            <h5>Doctors</h5>
-                            <div className="admin-search-box">
-                                <input type="text" placeholder='Search...' value={name} onChange={(e) => setName(e.target.value)} />
-                                <button><RiSearch2Line /></button>
+
+                        <div className="admin-container">
+                            <div className="container-header">
+                                <h5>Doctors</h5>
+                                <div className="admin-search-box">
+                                    <input type="text" placeholder='Search...' value={name} onChange={(e) => setName(e.target.value)} />
+                                    <button><RiSearch2Line /></button>
+                                </div>
+                                {/* <button className='btn btn-dark' onClick={addDoctor}>Add Doctor</button> */}
                             </div>
-                            {/* <button className='btn btn-dark' onClick={addDoctor}>Add Doctor</button> */}
+                            <Row>
+
+                                <Container fluid>
+                                    <Row>
+
+                                        <Col md={6} >
+                                            <div className="report-box">
+                                                <div className="report-input">
+                                                    <h5>Choose Date</h5>
+                                                </div>
+                                                <div className="report-input">
+                                                    <div className="report-input-row">
+                                                        {/* <div className="report-input-radio">
+                                                            <input type="radio" id='thisweek' name='date' />
+                                                            <label htmlFor="thisweek">This Week</label>
+                                                        </div> */}
+                                                        <div className="report-input-radio">
+                                                            <input type="radio" id='lastweek' name='date' />
+                                                            <label htmlFor="lastweek">Last Week</label>
+                                                        </div>
+                                                    </div>
+                                                    <div className="report-input-row">
+                                                        <div className="report-input-radio">
+                                                            <input type="radio" id='thismonth' name='date' />
+                                                            <label htmlFor="thismonth">This Month</label>
+                                                        </div>
+                                                        <div className="report-input-radio">
+                                                            <input type="radio" id='lastmonth' name='date' />
+                                                            <label htmlFor="lastmonth">Last month</label>
+                                                        </div>
+                                                    </div>
+                                                    <div className="report-input-row">
+                                                        <div className="report-input-radio">
+                                                            <input type="radio" id='thisyear' name='date' />
+                                                            <label htmlFor="thisyear">This Year</label>
+                                                        </div>
+                                                        <div className="report-input-radio">
+                                                            <input type="radio" id='lastyear' name='date' />
+                                                            <label htmlFor="lastyear">Last Year</label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="report-input">
+                                                    <div className="report-input-date-picker">
+                                                        <h5>Choose time</h5>
+                                                    </div>
+                                                    <div className="report-input-row">
+                                                        <div className="report-input-date-picker">
+                                                            <MobileDatePicker className='w-100' />
+                                                        </div>
+                                                        <div className="report-input-date-picker">
+                                                            <MobileDatePicker className='w-100' />
+                                                        </div>
+                                                    </div>
+                                                    
+                                                    {/* <div className="report-input-date-picker">
+                                                    <input type="radio" id='lastweek' name='date' />
+                                                    <label htmlFor="lastweek">Last Week</label>
+                                                </div> */}
+
+                                                </div>
+
+                                            </div>
+
+                                        </Col>
+                                        <Col md={6} >
+                                            <div className="report-box total">
+                                                <div className="report-box-item">
+                                                    <h6>Total</h6>
+                                                    <h3>4500</h3>
+                                                </div>
+                                                <div className="report-box-item">
+                                                    <h6>Total</h6>
+                                                    <h3>4500</h3>
+                                                </div>
+                                                <div className="report-box-item">
+                                                    <h6>Total</h6>
+                                                    <h3>4500</h3>
+                                                </div>
+
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+
+                                        <Col md={6} >
+                                            <Table className='table-main' responsive>
+                                                <thead>
+                                                    <tr>
+                                                        <th>#</th>
+                                                        <th>Patient Name</th>
+                                                        <th>Doctor</th>
+                                                        <th>Fees</th>
+                                                        <th>Date</th>
+                                                        <th>Time</th>
+                                                        <th>option</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {
+                                                        bookingList.map((item, index) => {
+                                                            return <tr key={index}>
+                                                                <td>{index + 1}</td>
+                                                                <td>
+                                                                    {/* <Link to={"/doctor/"+item._id}> */}
+                                                                    {item.patientName}
+                                                                    {/* </Link> */}
+                                                                </td>
+                                                                <td>{item.doctorId.name}</td>
+                                                                <td>{item.fees}</td>
+                                                                <td>{new Date(item.date).toLocaleDateString()}</td>
+                                                                <td>{new Date(item.time).toLocaleTimeString('en-US')}</td>
+                                                                <td className='option-btn'>
+                                                                    <Dropdown>
+                                                                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                                                            <RiMore2Fill />
+                                                                        </Dropdown.Toggle>
+
+                                                                        <Dropdown.Menu>
+                                                                            {/* <Dropdown.Item href="#" onClick={() => { navigate('/account/hospital/schedule/'+item._id) }}>Schedule</Dropdown.Item> */}
+                                                                        </Dropdown.Menu>
+                                                                    </Dropdown>
+                                                                </td>
+                                                            </tr>
+                                                        })
+                                                    }
+                                                </tbody>
+                                            </Table>
+                                        </Col>
+                                        <Col md={6} >
+                                            <Table className='table-main' responsive>
+                                                <thead>
+                                                    <tr>
+                                                        <th>#</th>
+                                                        <th>Total</th>
+                                                        <th>Count</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {
+                                                        bookingList.map((item, index) => {
+                                                            return <tr key={index}>
+                                                                <td>{index + 1}</td>
+                                                                <td>
+                                                                    {/* <Link to={"/doctor/"+item._id}> */}
+                                                                    {item.patientName}
+                                                                    {/* </Link> */}
+                                                                </td>
+                                                                <td>{item.doctorId.name}</td>
+
+                                                            </tr>
+                                                        })
+                                                    }
+                                                </tbody>
+                                            </Table>
+                                        </Col>
+                                    </Row>
+
+                                </Container>
+                            </Row>
+
+
+
                         </div>
-                        <Container fluid>
-                            <Col md={6} >
-                        <Table className='table-main' responsive>
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Patient Name</th>
-                                    <th>Doctor</th>
-                                    <th>Fees</th>
-                                    <th>Date</th>
-                                    <th>Time</th>
-                                    <th>option</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    bookingList.map((item, index) => {
-                                        return <tr key={index}>
-                                            <td>{index + 1}</td>
-                                            <td>
-                                                {/* <Link to={"/doctor/"+item._id}> */}
-                                                {item.patientName}
-                                                {/* </Link> */}
-                                            </td>
-                                            <td>{item.doctorId.name}</td>
-                                            <td>{item.fees}</td>
-                                            <td>{new Date(item.date).toLocaleDateString()}</td>
-                                            <td>{new Date(item.time).toLocaleTimeString('en-US')}</td>
-                                            <td className='option-btn'>
-                                                <Dropdown>
-                                                    <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                                                        <RiMore2Fill />
-                                                    </Dropdown.Toggle>
+                    </LocalizationProvider>
 
-                                                    <Dropdown.Menu>
-                                                        {/* <Dropdown.Item href="#" onClick={() => { navigate('/account/hospital/schedule/'+item._id) }}>Schedule</Dropdown.Item> */}
-                                                    </Dropdown.Menu>
-                                                </Dropdown>
-                                            </td>
-                                        </tr>
-                                    })
-                                }
-                            </tbody>
-                        </Table>
-                        </Col>
-                 </Container>
-       
-
-                    </div>
                 </Container>
             </div>
             <Backdrop
