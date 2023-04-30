dd33b1ddcb508b0d6d215d5a7b3996da8e4e164c
fasilkp <fasilkp314@gmail.com>
2023-04-30 12:13:10 +0530

    some change

---
+++ client/build/precache-manifest.921c9fdaa98e8dd69b32d1c636fcbb1d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad19226dc276f2677d66",
+    "url": "/static/css/main.baffdf04.chunk.css"
+  },
+  {
+    "revision": "ad19226dc276f2677d66",
+    "url": "/static/js/main.ad19226d.chunk.js"
+  },
+  {
+    "revision": "105d6147b2deb0c0abce",
+    "url": "/static/css/1.89fd01e7.chunk.css"
+  },
+  {
+    "revision": "105d6147b2deb0c0abce",
+    "url": "/static/js/1.105d6147.chunk.js"
+  },
+  {
+    "revision": "38ae571a612348375a98",
+    "url": "/static/js/2.38ae571a.chunk.js"
+  },
+  {
+    "revision": "47ac83b0f05779002c37",
+    "url": "/static/js/3.47ac83b0.chunk.js"
+  },
+  {
+    "revision": "bb2ab2271d675e5a1949",
+    "url": "/static/js/4.bb2ab227.chunk.js"
+  },
+  {
+    "revision": "e20948e726d0cbd9efd9",
+    "url": "/static/js/runtime~main.e20948e7.js"
+  },
+  {
+    "revision": "3c3b44978896ac1b1130883022d5060f",
+    "url": "/static/media/login.3c3b4497.jpg"
+  },
+  {
+    "revision": "e9f39ee34a07386e5cdcc266deb8d87e",
+    "url": "/static/media/otp.e9f39ee3.webp"
+  },
+  {
+    "revision": "fc2256fcfb1198dabc304b4d7d0ef160",
+    "url": "/static/media/no-result.fc2256fc.png"
+  },
+  {
+    "revision": "c8e16a941464362f646f6f223eddbb5b",
+    "url": "/static/media/emailSent.c8e16a94.jpg"
+  },
+  {
+    "revision": "ed046882d2d8db98da134deed2d5edf1",
+    "url": "/static/media/resetPassword.ed046882.jpg"
+  },
+  {
+    "revision": "0b42b2b8efeef779a405a62f0b0ef793",
+    "url": "/static/media/doctor.0b42b2b8.png"
+  },
+  {
+    "revision": "23c449a2b32a58d9b7868eb7287bcfbc",
+    "url": "/static/media/notFound.23c449a2.png"
+  },
+  {
+    "revision": "c64099aa3d47208eb2c88ceb99d7277b",
+    "url": "/static/media/hospital.c64099aa.jpg"
+  },
+  {
+    "revision": "9024d486251cf2457e4be1d9d37d9ced",
+    "url": "/static/media/01.9024d486.jpg"
+  },
+  {
+    "revision": "447712408552e0e930e45ab85ac4d69c",
+    "url": "/static/media/user-banner.44771240.png"
+  },
+  {
+    "revision": "82ab5af2576cc116287273db4c18f756",
+    "url": "/index.html"
+  }
+];
