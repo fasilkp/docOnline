26ef8325cf4cab3e8c74f8a2e343a8c03ba87eae
fasilkp <fasilkp314@gmail.com>
2023-06-01 17:45:21 +0530

    user chats controller created

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -2,12 +2,25 @@ import ChatModel from "../models/ChatModel.js";
 
 export const createChat = async (req, res) => {
     const newChat = new ChatModel({
-      members: [req.body.senderId, req.body.receiverId],
+        members: [req.body.senderId, req.body.receiverId],
     });
     try {
-      const result = await newChat.save();
-      res.status(200).json(result);
+        const result = await newChat.save();
+        res.json({ err: false, result });
     } catch (error) {
-      res.status(500).json(error);
+        res.json({ err: true });
     }
-  };
+};
+
+
+export const userChats = async (req, res) => {
+    try {
+        const chat = await ChatModel.find({
+            members: { $in: [req.params.userId] },
+        });
+        res.json({err:false, chat})
+    } catch (error) {
+        res.json({ err: true });
+
+    }
+};
