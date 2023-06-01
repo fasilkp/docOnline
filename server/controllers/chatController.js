383e5a4f5afdaff83a4427feeb97f8b88f8efb04
fasilkp <fasilkp314@gmail.com>
2023-06-01 17:46:16 +0530

    find chat controller created

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -24,3 +24,14 @@ export const userChats = async (req, res) => {
 
     }
 };
+
+export const findChat = async (req, res) => {
+    try {
+      const chat = await ChatModel.findOne({
+        members: { $all: [req.params.firstId, req.params.secondId] },
+      });
+      res.json({err:false, chat})
+    } catch (error) {
+        res.json({ err: true });
+    }
+  };
