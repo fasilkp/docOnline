ed74dcb084b9877f95872bfa3e837175f042f9ef
fasilkp <fasilkp314@gmail.com>
2023-06-01 17:43:48 +0530

    create chat created

---
+++ server/controllers/chatController.js
@@ -0,0 +1,13 @@
+import ChatModel from "../models/ChatModel.js";
+
+export const createChat = async (req, res) => {
+    const newChat = new ChatModel({
+      members: [req.body.senderId, req.body.receiverId],
+    });
+    try {
+      const result = await newChat.save();
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  };
