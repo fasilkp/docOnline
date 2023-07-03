a92de6b5cac9299720ac19d30b05f2b3cc6cdc98
fasilkp <fasilkp314@gmail.com>
2023-07-03 20:14:50 +0530

    env updated

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 function dbConnect(){
-    mongoose.connect("mongodb://127.0.0.1/docOnline").then(result=>{
+    mongoose.connect(process.env.DB).then(result=>{
         console.log("Database connected")
     }).catch((err)=>{
         console.log("data base error \n"+err)
